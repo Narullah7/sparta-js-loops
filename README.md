6b91403fac762fc76fcae1a89dcf5f7ddf0e47b1
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2018-01-10 21:57:23 +0000

    corrected mistakes and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# sparta-js-loops
+# Sparta JS loops
+
+In this folder I look at all the loops available in JavaScript e.g. for loop, while loop, do while loop, for in loop and finally I completed the Fizzbuzz challenge.
+
+To see the results, once you have opened ```index.html``` in the browser right-click and click 'inspect' then finally click on console
--- main.js
+++ main.js
@@ -1,40 +1,42 @@
 // for loop
-// var array = ["Richard","Joe","Lexie"];
-// // debugger
-// for (var i = 0; i< array.length; i++) {
-//   console.log(array[i]);
-// }
+console.log("----- For loop ------");
+var array = ["Richard","Joe","Lexie"];
 
+for (var i = 0; i< array.length; i++) {
+  console.log(array[i]);
+}
+console.log("------- While ------------");
 //while
-// var j = 0;
-//
-// while (j<5) {
-//   console.log("This loop has run " + j + " times");
-//   j++
-// }
+var j = 0;
+
+while (j<5) {
+  console.log("This loop has run " + j + " times");
+  j++
+}
+console.log("------ Do while ---------");
 
 // do while - the loop will always run the code once before checking the condition
-// var number;
+var number;
 //
-// do {
-//   number = Math.random();
-//   console.log(number);
-// } while (number < 0.5);
-//
+do {
+  number = Math.random();
+  console.log(number);
+} while (number < 0.5);
 
+console.log("----- For In -------");
 // for in
-// var myobject = new Object();
+var myobject = new Object();
 //
-// myobject.a = "1";
-// myobject.b = "2";
-// myobject.c = "3";
+myobject.a = "1";
+myobject.b = "2";
+myobject.c = "3";
 //
-// for (var key in myobject) {
-//   console.log(key);
-//   console.log(myobject[key]);
-// }
+for (var key in myobject) {
+  console.log(key);
+  console.log(myobject[key]);
+}
 
-// fizzbuzz challenge.
+console.log("------ fizzbuzz challenge -------")
 for (var i = 1; i<100; i++) {
   if ((i % 3 === 0) && ( i % 5 === 0) ){
     console.log("fizzbuzz");
